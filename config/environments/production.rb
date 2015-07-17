573f08b26ec6ecd9d705237af485a139ccc22081
Max Plomer <maxplomer@gmail.com>
2015-07-17 14:51:28 -0400

    wrong bucket

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,7 +90,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => 'spree-gramercy-development'
+      :bucket => 'spree-gramercy'
     }
   }
 end
