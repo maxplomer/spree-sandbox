f2c889ad7a6d6a04b435e7288ae23621e8b01076
Max Plomer <maxplomer@gmail.com>
2015-07-17 22:44:26 -0400

    create Api::Digitals controller

---
+++ app/controllers/api/digitals_controller.rb
@@ -0,0 +1,9 @@
+class Api::DigitalsController < ApplicationController
+
+  def create
+    logger.info("HELLOWORLDHELLOWORLDHELLOWORLD")
+    logger.info(params)
+    logger.info("HELLOWORLDHELLOWORLDHELLOWORLD")
+  end
+
+end
---
+++ app/helpers/api/digitals_helper.rb
@@ -0,0 +1,2 @@
+module Api::DigitalsHelper
+end
