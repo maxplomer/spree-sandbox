2e6f55396eedddbca8dfdb75a4b7e09d140006dc
Max Plomer <maxplomer@gmail.com>
2015-07-17 23:19:36 -0400

    can create digital product from api

--- app/controllers/api/digitals_controller.rb
+++ app/controllers/api/digitals_controller.rb
@@ -1,10 +1,23 @@
 class Api::DigitalsController < ApplicationController
 
   def create
-    logger.info("HELLOWORLDHELLOWORLDHELLOWORLD")
-    logger.info(params)
-    logger.info("HELLOWORLDHELLOWORLDHELLOWORLD")
-    render json: {hello: "world"} 
+    product_id = params["product_id"]
+    @digital = Spree::Digital.new(digital_params)
+    @digital.variant_id = product_id
+
+    if @digital.save
+      render json: @digital
+    else
+      render :json => @digital.errors, :status => :unprocessable_entity
+    end
+  end
+
+  private
+
+  def digital_params
+    params.require(:digital).permit(:attachment)
   end
 
 end
+
+## TODO before_action check for api key
